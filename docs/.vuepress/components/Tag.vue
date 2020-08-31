046bb7911154a3e41100dc7a4a20f512fa11d5d6
curly <water.curly@gmail.com>
2020-08-31 21:40:38 +0800

    build(component): 增加标签组件

---
+++ docs/.vuepress/components/Tag.vue
@@ -0,0 +1,21 @@
+<template>
+  <span class="tag"><slot></slot></span>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+.tag {
+    display: inline-block;
+    border-radius: 4px;
+    padding: 0 4px;
+    color: #666;
+    background: #ffd400;
+    transform: scale(0.8);
+}
+</style>
+
