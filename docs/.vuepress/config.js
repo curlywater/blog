83312bebda5b65827e7395c30b90e2d5a31a83d8
curly <water.curly@gmail.com>
2020-09-01 10:02:39 +0800

    update(algo): 《排序》

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -82,7 +82,7 @@ const front2EndSidebar = [
   },
 ];
 
-const algorithmSidebar = ["/算法/", "/算法/复杂度计算/", "/算法/基础数据结构/", "/算法/递归/", "/算法/排序/介绍.md", "/算法/排序/分析.md"];
+const algorithmSidebar = ["/算法/", "/算法/复杂度计算/", "/算法/基础数据结构/", "/算法/递归/", "/算法/排序/"];
 
 const exploreSidebar = [{
   title: "Git",
